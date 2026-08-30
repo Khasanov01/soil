cf2e7a71810929c0d30505323aaee2df3365dd53
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-30 17:29:59 +0500

    feat:develop message API request

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ app=FastAPI(title='landing FastAPI soil project')
 async def get_greeting(request: Request) -> str :
     print("request:", request)
     return "Hello from starlette"
+
+@app.get("/message", response_model=dict)
+async def get_message():
+    return {"message" : "Hi, MIT"}
