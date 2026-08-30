1d8365fd327601964467e10593086984b585da0b
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-30 22:41:53 +0500

    feat: develop get student rest api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import Request, Response
+from provider import students
 """FastAPI uses Starlette Framework by default"""
 
 print("\n Backend server is running \n")
@@ -16,3 +17,10 @@ async def get_greeting(request: Request) -> str :
 @app.get("/message", response_model=dict)
 async def get_message():
     return {"message" : "Hi, MIT"}
+
+
+#FastAPI handler JSON
+@app.get("/mit/all", response_model=dict[int, dict])
+def get_students():
+    #model>database > data
+    return students
---
+++ provider.py
@@ -0,0 +1,8 @@
+#database
+
+students={
+    3 : {"id": 3, "name": "Justin", "age": 26, "category":"MERN"},
+    12 : {"id": 3, "name": "David", "age": 28, "category":"MERN"},
+    14 : {"id": 3, "name": "Max", "age": None, "category":"PYTHON"},
+    23 : {"id": 3, "name": "Justin", "age": 22, "category":"PYTHON"},
+}
