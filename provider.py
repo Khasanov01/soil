f75702ae31417a01c2f826136d26db96f2186627
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-30 22:49:39 +0500

    feat: learn Enums data type

--- provider.py
+++ provider.py
@@ -1,8 +1,14 @@
-#database
+from enum import Enum
+# Enums
+class Category(Enum):
+    MERN = "MERN NESTJS Fullstack"
+    PYTHON= "AI python Fullstack"
+
+# Database
 
 students={
-    3 : {"id": 3, "name": "Justin", "age": 26, "category":"MERN"},
-    12 : {"id": 3, "name": "David", "age": 28, "category":"MERN"},
-    14 : {"id": 3, "name": "Max", "age": None, "category":"PYTHON"},
-    23 : {"id": 3, "name": "Justin", "age": 22, "category":"PYTHON"},
+    3 : {"id": 3, "name": "Justin", "age": 26, "category": Category.MERN},
+    12 : {"id": 3, "name": "David", "age": 28, "category":Category.MERN},
+    14 : {"id": 3, "name": "Max", "age": None, "category":Category.PYTHON},
+    23 : {"id": 3, "name": "Justin", "age": 22, "category":Category.PYTHON},
 }
